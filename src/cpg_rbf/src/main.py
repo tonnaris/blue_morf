74617f7e7349ad65bbbf0a6c3cfadb652f947bd0
naris <tonnaris@gmail.com>
2022-09-05 13:11:49 +0200

    commit

--- src/cpg_rbf/src/main.py
+++ src/cpg_rbf/src/main.py
@@ -16,14 +16,17 @@ def main():
     pub_dynamixel = rospy.Publisher('set_position', Int32MultiArray, queue_size=1)
     rospy.init_node('main', anonymous=True)
     rate = rospy.Rate(60) 
-    cpg = CPG()
-    cpg.set_frequency()
+    cpg_walk = CPG()
+    cpg_walk.set_frequency()
+    cpg_breathe = CPG()
+    cpg_breathe.set_frequency()
     mapping = Motormapping_angle()
     TORQUE = 0
     dynamixel_positon = [0]*19 
     motion = "set"
     speed = "sigma"
     sigma = 0.03
+    alpha = 0.03
     timer_state = True
     time_t0 = time.perf_counter() # seconds
     arduino_control = [0,0]
@@ -42,32 +45,36 @@ def main():
 
         if motion == "set":
             dynamixel_positon = mapping.map([0,0,0,1,1]) 
-            cpg = CPG()
-            cpg.set_frequency(sigma * np.pi)
+            cpg_walk = CPG()
+            cpg_walk.set_frequency(sigma * np.pi)
         elif motion == "forward":
-            signal_leg[0] = cpg_data[0]
-            signal_leg[1] = cpg_data[1] 
-            signal_leg[2] = -cpg_data[1]
+            signal_leg[0] = cpg_walk_data[0]
+            signal_leg[1] = cpg_walk_data[1] 
+            signal_leg[2] = -cpg_walk_data[1]
             signal_leg[3] = 1
             signal_leg[4] = 1
         elif motion == "backward":
-            signal_leg[0] = -cpg_data[0]
-            signal_leg[1] = cpg_data[1] 
-            signal_leg[2] = -cpg_data[1]
+            signal_leg[0] = -cpg_walk_data[0]
+            signal_leg[1] = cpg_walk_data[1] 
+            signal_leg[2] = -cpg_walk_data[1]
             signal_leg[3] = 1
             signal_leg[4] = 1
         elif motion == "left":
-            signal_leg[0] = cpg_data[0]
-            signal_leg[1] = cpg_data[1] 
-            signal_leg[2] = -cpg_data[1]
-            signal_leg[3] = -1
+            signal_leg[0] = cpg_walk_data[0]
+            signal_leg[1] = cpg_walk_data[1] 
+            signal_leg[2] = -cpg_walk_data[1]
+            signal_leg[3] = 0.7
             signal_leg[4] = 1
+            # signal_leg[3] = -1
+            # signal_leg[4] = 1
         elif motion == "right":
-            signal_leg[0] = cpg_data[0]
-            signal_leg[1] = cpg_data[1] 
-            signal_leg[2] = -cpg_data[1]
+            signal_leg[0] = cpg_walk_data[0]
+            signal_leg[1] = cpg_walk_data[1] 
+            signal_leg[2] = -cpg_walk_data[1]
             signal_leg[3] = 1
-            signal_leg[4] = -1
+            signal_leg[4] = 0.7
+            # signal_leg[3] = 1
+            # signal_leg[4] = -1
         elif motion == "stop":
             if count_change > 0:
                 signal_leg[0] *= count_change
@@ -84,43 +91,77 @@ def main():
                 signal_leg[2] *= count_change
                 count_change += 0.01
             dynamixel_positon = mapping.map([signal_leg[0],signal_leg[1],signal_leg[2],signal_leg[3],signal_leg[4]])
-            cpg_data = np.array(cpg.update())
+            cpg_walk_data = np.array(cpg_walk.update())
 
 
         if speed == "+sigma":
             sigma += 0.0001
             if sigma >= 0.06: sigma = 0.06
-            cpg.set_frequency(sigma * np.pi)
+            cpg_walk.set_frequency(sigma * np.pi)
         elif speed == "-sigma":
             sigma -= 0.0001
             if sigma <= 0.01: sigma = 0.01
-            cpg.set_frequency(sigma * np.pi)
+            cpg_walk.set_frequency(sigma * np.pi)
   
         
         print("sigma %.4f"%sigma)
+        learning_rate = 0.01
+        forgeting_rate = 0.01
+        if motion == "stop"
+            state = 0
+        else
+            state = 1
+        alpha = (learning_rate * state * (sigma + alpha)) + (forgeting_rate * (state-1) * (alpha**2))
 
         if motion == "set":
             arduino_control = [0,0]
-            time_t0 = time.perf_counter()
-            gamma = 5
-        if motion == "stop":
-            gamma -= 0.002
+            cpg_breathe = CPG()
+            cpg_breathe.set_frequency()
+        elif motion == "stop":
+            alpha -= 0.002
+            cpg_breathe.set_frequency(alpha * np.pi)
         else:
             gamma += 0.001
-            if gamma >= 10:
-                gamma = 10
+            if alpha >= 0.1:
+                alpha = 0.1
+            cpg_breathe.set_frequency(alpha * np.pi)
 
-            time_t1 = time.perf_counter()
-            count_time = time_t1 - time_t0
-
-            if timer_state == True and count_time  >= gamma * set_duration:
-                arduino_control = [1,1]
+        if motion != "set":
+            cpg_breathe_data = np.array(cpg_breathe.update())
+            if timer_state == True and cpg_breathe_data > 0:
+                arduino_control = [1,0]
                 timer_state = False
                 time_t0 = time_t1
-            elif timer_state == False and count_time  >= gamma * (1-set_duration):
-                arduino_control = [0,0]
+            elif timer_state == False and cpg_breathe_data < 0:
+                arduino_control = [0,1]
                 timer_state = True
                 time_t0 = time_t1
+
+
+
+
+        # if motion == "set":
+        #     arduino_control = [0,0]
+        #     time_t0 = time.perf_counter()
+        #     gamma = 5
+        # if motion == "stop":
+        #     gamma -= 0.002
+        # else:
+        #     gamma += 0.001
+        #     if gamma >= 10:
+        #         gamma = 10
+
+        #     time_t1 = time.perf_counter()
+        #     count_time = time_t1 - time_t0
+
+        #     if timer_state == True and count_time  >= gamma * set_duration:
+        #         arduino_control = [1,1]
+        #         timer_state = False
+        #         time_t0 = time_t1
+        #     elif timer_state == False and count_time  >= gamma * (1-set_duration):
+        #         arduino_control = [0,0]
+        #         timer_state = True
+        #         time_t0 = time_t1
 
                 
         
